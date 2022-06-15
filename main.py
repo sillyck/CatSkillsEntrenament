b489f11f89cbcd7856a14b3d3a4bbd48174f71d9
Jordi Ribellas <ribellas2002@gmail.com>
2022-06-15 15:36:37 +0200

    Codi base del robot

---
+++ main.py
@@ -0,0 +1,14 @@
+from serial import Serial
+
+Robot = Serial("COM4", 115200, timeout=.1)
+Robot.flushInput()
+Robot.flushOutput()
+
+endavant = "cmd_vel[0.1,0,0]"
+endarrere = "cmd_vel[0.1,0,0]"
+dreta = "cmd_vel[0.1,0,0]"
+esquerra = "cmd_vel[0.1,0,0]"
+
+Robot.write(endavant.encode() + '\n'.encode())
+getValue = Robot.readline()
+print(getValue)
